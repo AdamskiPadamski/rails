b2d953a18afd616549803e50385d2ca54aaf5cd5
AdamskiPadamski <adam.t.wright@outlook.com>
2015-06-29 13:35:27 +0100

    Added model tests

--- anisite/test/models/user_test.rb
+++ anisite/test/models/user_test.rb
@@ -4,4 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "no save without email" do
+    user = User.new
+    assert_not user.save
+  end
+  
+  test "password length between 8 and 72" do
+    user = User.new(email:"test",password:"testtest",password_confirmation:"testtest")
+    assert user.save
+  end
+  
+  test "password length incorrect" do
+    user = User.new(email:"test",password:"t",password_confirmation:"t")
+    assert_not user.save
+  end
+  
 end
