cf2a23b99449cc76e6e3d8ef05fd320191b6a4c9
AdamskiPadamski <adam.t.wright@outlook.com>
2015-07-01 09:58:19 +0100

    Add username to user whitelist

--- anisite/app/models/user.rb
+++ anisite/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, presence:true
+  validates :username, presence:true, uniqueness:true
   validates :password, length: { in: 8..72 }
   has_many :observations
   has_many :animes, through: :observations
